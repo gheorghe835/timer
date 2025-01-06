f77a9a96980d5a03b35a1221849e6e6d4e6e9753
gheorghe835 <gheorghe835@gmail.com>
2025-01-06 13:16:52 +0200

    Add class: CountdownManager implements countdown logic.

--- src/timerApp/CountdownManager.java
+++ src/timerApp/CountdownManager.java
@@ -2,12 +2,70 @@ package timerApp;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.TimerTask;
 
 public class CountdownManager {
-    private static Timer countdownTimer;
-    private static int countdownValue = 10;//The default countdown
-    private static Color chosenColor = Color.BLUE;
-    private static JFrame colorChangingFrame;
-    private static int colorSpeed = 1000;//Rate of color change (ms)
+   private Timer countdownTimer;
+   private int countdownValue = 10;
+   private Color chooseColor = Color.BLUE;
+   private JFrame colorChangingFrame;
+   private boolean toggleColor = true;
 
+    public int getCountdownValue() {
+        return countdownValue;
+    }
+
+    public void chooseColor(JFrame parentFrame){
+        Color color = JColorChooser.showDialog(parentFrame,"  Choose Color",chooseColor);
+        if (color != null){
+            chooseColor = color;
+        }
+    }
+
+    public void startCountdown(int countdownValue,int speed, JFrame parenrFrame){
+        this.countdownValue = countdownValue;
+
+        if (colorChangingFrame == null){
+            colorChangingFrame = new JFrame("  Color Changig Window");
+            colorChangingFrame.setSize(400,400);
+            colorChangingFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            colorChangingFrame.setVisible(true);
+        }
+
+        countdownTimer = new Timer();
+        countdownTimer.scheduleAtFixeRate(new TimerTask(){
+            @Override
+            public void run() {
+                if (CountdownManager.this.countdownValue > 0) {
+                    CountdownManager.this.countdownValue--;
+                    toggleBackgroundColor();
+                } else {
+                    countdownTimer.cancel();
+                    Utils.disableControls(parentFrame, true);
+                }
+            }
+        }, 0, speed);
+    }
+
+    public void stopCountdown(){
+        if (countdownTimer != null){
+            countdownTimer.cancel();
+        }
+        if (colorChangingFrame != null){
+            colorChangingFrame.dispose();
+            colorChangingFrame = null;
+        }
+    }
+
+    public void toggleBackgroundColor(){
+        SwingUtilities.invokeLater(() -> {
+            if (toggleColor){
+                colorChangingFrame.getContentPane().setBackground(chooseColor);
+            }
+            else {
+                colorChangingFrame.getContentPane().setBackground(Color.WHITE);
+            }
+            toggleColor = !toggleColor;
+        });
+    }
 }
