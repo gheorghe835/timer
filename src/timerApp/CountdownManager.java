37fcd3f92196b1672219d90b4aa4043b52203516
gheorghe835 <gheorghe835@gmail.com>
2025-01-06 07:44:00 +0200

    Add the TimerApp class that starts the application.

---
+++ src/timerApp/CountdownManager.java
@@ -0,0 +1,13 @@
+package timerApp;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CountdownManager {
+    private static Timer countdownTimer;
+    private static int countdownValue = 10;//The default countdown
+    private static Color chosenColor = Color.BLUE;
+    private static JFrame colorChangingFrame;
+    private static int colorSpeed = 1000;//Rate of color change (ms)
+
+}
--- src/timerApp/TimerApp.java
+++ src/timerApp/TimerApp.java
@@ -1,4 +1,27 @@
 package timerApp;
 
+import javax.swing.*;
+
 public class TimerApp {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() ->{
+            int choice = JOptionPane.showOptionDialog(
+                    null,
+                    "  Choose an option",
+                    "  Opyion panel",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    new Object[]{"  Settings","  Close"},
+                    "  Settings"
+            );
+
+            if (choice == JOptionPane.YES_NO_OPTION){
+                new SettingsWindow();//Opens the settings window
+            }
+            else {
+                System.exit(0);
+            }
+        });
+    }
 }
